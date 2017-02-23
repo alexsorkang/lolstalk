2af7c30d14c80b26fef246499a80086a6d2b14bb
alex sor kang <asork42@berkeley.edu>
2017-02-23 14:56:58 -0800

    server now searches twitter and gets names

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var lol = require('leagueapi');
+var twitter = require('twit');
+ 
 
 // var lolClient = lol.client({
     // apiKey: 'RGAPI-ed53eab6-4dd0-4b4f-9d82-41c356030dd8'
@@ -11,9 +13,19 @@ var lol = require('leagueapi');
 //     lolClient.destroy();
 // });
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var name = 'doublelift';
+router.get('/', (req, res, next) => {
+
+  var t = new twitter({
+    consumer_key: 'kjAkJECqGqGgbp8rTjKb3g7t8',
+    consumer_secret: 'zgCuZYJXaH4ACLRzJK3gsZFw5S9IrrXEVkQr72sWY09IDSgz8Z',
+    app_only_auth: true
+  });
+
+// https://api.twitter.com/1.1/users/show.json?screen_name=twitterdev
+
+  var name = 'bobjenkins';
   var region = 'na';
   lol.init('RGAPI-ed53eab6-4dd0-4b4f-9d82-41c356030dd8', 'na')
   lol.Summoner.getByName(name, region, (err,data) => {
@@ -21,7 +33,10 @@ router.get('/', function(req, res, next) {
     lol.getCurrentGame(id, region, (err1, data1) => {
       // console.log(data1);
       data1.participants.map((x) => {
-        console.log(x.summonerName)
+        // console.log(x.summonerName)
+        t.get('users/show', {screen_name: x.summonerName}, (err, data, response) => {
+          console.log(x.summonerName + ": "+ data.name);  // 
+        });
       })
     });
   });
